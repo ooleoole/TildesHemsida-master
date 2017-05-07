220c3a810837d9182b5e0f88ae4fa7ff3d759ab9
Ola Popp <olapopp85@gmail.com>
2017-05-07 13:19:45 +0200

    Add project files.

---
+++ TildesHemsida/Scripts/Script.js
@@ -0,0 +1,61 @@
+var FarmSearch;
+(function (FarmSearch_1) {
+    window.onload = function () {
+        var farmSearch = new FarmSearch();
+        document.getElementById("inputField").focus();
+        farmSearch.init("inputField", "searchButton", "body");
+    };
+    var FarmSearch = (function () {
+        function FarmSearch() {
+            var _this = this;
+            this.cowPasture = "/Images/kor-majorns-hage.JPG";
+            this.otefield = "/Images/staket-i-hage.jpg";
+            this.cat = "/Images/cat.jpg";
+            this.farm = "/Images/farm.jpg";
+            this.farmer = "/Images/farmer.jpg";
+            this.hay = "/Images/hay.jpg";
+            this.horse = "/Images/horse.jpg";
+            this.tractor = "/Images/john-deere-traktor.jpg";
+            this.pig = "/Images/pig.jpg";
+            this.tilde = "/Images/Tilde.jpg";
+            this.imageElement = document.getElementById("image");
+            this.getImageOnEnter = function (event) {
+                var e = event;
+                if (e.key === "Enter") {
+                    _this.getImage();
+                }
+            };
+            this.getImage = function () {
+                var inputElement = document.getElementById("inputField");
+                var searchPhrase = inputElement.value;
+                if (searchPhrase.toLowerCase() === "ko")
+                    _this.imageElement.src = _this.cowPasture;
+                if (searchPhrase.toLowerCase() === "hage")
+                    _this.imageElement.src = _this.otefield;
+                if (searchPhrase.toLowerCase() === "katt")
+                    _this.imageElement.src = _this.cat;
+                if (searchPhrase.toLowerCase() === "farm")
+                    _this.imageElement.src = _this.farm;
+                if (searchPhrase.toLowerCase() === "bonde")
+                    _this.imageElement.src = _this.farmer;
+                if (searchPhrase.toLowerCase() === "hö")
+                    _this.imageElement.src = _this.hay;
+                if (searchPhrase.toLowerCase() === "traktor")
+                    _this.imageElement.src = _this.tractor;
+                if (searchPhrase.toLowerCase() === "gris")
+                    _this.imageElement.src = _this.pig;
+                if (searchPhrase.toLowerCase() === "häst")
+                    _this.imageElement.src = _this.horse;
+                if (searchPhrase.toLowerCase() === "tilde")
+                    _this.imageElement.src = _this.tilde;
+            };
+        }
+        FarmSearch.prototype.init = function (inputField, searchButton, body) {
+            document.getElementById(inputField).addEventListener("keydown", this.getImageOnEnter, false);
+            document.getElementById(searchButton).addEventListener("click", this.getImage, false);
+            document.getElementById(body).addEventListener("keydown", this.getImageOnEnter, false);
+        };
+        return FarmSearch;
+    }());
+})(FarmSearch || (FarmSearch = {}));
+//# sourceMappingURL=Script.js.map
---
+++ TildesHemsida/Scripts/Script.ts
@@ -0,0 +1,67 @@
+﻿namespace FarmSearch {
+    window.onload = () => {
+        var farmSearch = new FarmSearch();
+        document.getElementById("inputField").focus();
+        farmSearch.init("inputField", "searchButton", "body");
+    };
+
+
+
+
+
+    class FarmSearch {
+        cowPasture: string = "/Images/kor-majorns-hage.JPG";
+        otefield: string = "/Images/staket-i-hage.jpg";
+        cat: string = "/Images/cat.jpg";
+        farm: string = "/Images/farm.jpg";
+        farmer: string = "/Images/farmer.jpg";
+        hay: string = "/Images/hay.jpg";
+        horse: string = "/Images/horse.jpg";
+        tractor: string = "/Images/john-deere-traktor.jpg";
+        pig: string = "/Images/pig.jpg";
+        tilde: string = "/Images/Tilde.jpg";
+        imageElement: HTMLImageElement = document.getElementById("image") as HTMLImageElement;
+
+        public init(inputField: string, searchButton: string, body: string): void {
+            document.getElementById(inputField).addEventListener("keydown", this.getImageOnEnter, false);
+            document.getElementById(searchButton).addEventListener("click", this.getImage, false);
+            document.getElementById(body).addEventListener("keydown", this.getImageOnEnter, false);
+        }
+
+        public getImageOnEnter = (event: Event) => {
+            var e = event as KeyboardEvent;
+            if (e.key === "Enter") {
+                this.getImage();
+            }
+        }
+
+        public getImage = () => {
+            var inputElement = document.getElementById("inputField") as HTMLInputElement;
+            var searchPhrase = inputElement.value;
+
+            if (searchPhrase.toLowerCase() === "ko")
+                this.imageElement.src = this.cowPasture;
+            if (searchPhrase.toLowerCase() === "hage")
+                this.imageElement.src = this.otefield;
+            if (searchPhrase.toLowerCase() === "katt")
+                this.imageElement.src = this.cat;
+            if (searchPhrase.toLowerCase() === "farm")
+                this.imageElement.src = this.farm;
+            if (searchPhrase.toLowerCase() === "bonde")
+                this.imageElement.src = this.farmer;
+            if (searchPhrase.toLowerCase() === "hö")
+                this.imageElement.src = this.hay;
+            if (searchPhrase.toLowerCase() === "traktor")
+                this.imageElement.src = this.tractor;
+            if (searchPhrase.toLowerCase() === "gris")
+                this.imageElement.src = this.pig;
+            if (searchPhrase.toLowerCase() === "häst")
+                this.imageElement.src = this.horse;
+            if (searchPhrase.toLowerCase() === "tilde")
+                this.imageElement.src = this.tilde;
+
+
+
+        }
+    }
+}
